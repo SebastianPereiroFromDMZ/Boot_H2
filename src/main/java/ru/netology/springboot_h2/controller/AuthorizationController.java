daf38d2e86888e85d4b7c5413bef525e8d97d57b
Kost Kuzmin <Kuzmin_konstantin@bk.ru>
2023-06-20 16:58:05 +0300

    commit 2

--- src/main/java/ru/netology/springboot_h2/controller/AuthorizationController.java
+++ src/main/java/ru/netology/springboot_h2/controller/AuthorizationController.java
@@ -1,8 +1,13 @@
 package ru.netology.springboot_h2.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.netology.springboot_h2.exception.InvalidCredentials;
+import ru.netology.springboot_h2.exception.UnauthorizedUser;
 import ru.netology.springboot_h2.model.Authorities;
 import ru.netology.springboot_h2.service.AuthorizationService;
 
@@ -12,8 +17,22 @@ import java.util.List;
 public class AuthorizationController {
     AuthorizationService service;
 
+    public AuthorizationController(AuthorizationService service){
+        this.service = service;
+    }
+
     @GetMapping("/authorize")
     public List<Authorities> getAuthorities(@RequestParam("user") String user, @RequestParam("password") String password) {
         return service.getAuthorities(user, password);
+    }
+
+    @ExceptionHandler(InvalidCredentials.class)
+    public ResponseEntity<String> iaeHandler(InvalidCredentials e) {
+        return new ResponseEntity<>("IllegalArgumentException in throwExceptionIne method", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UnauthorizedUser.class)
+    public ResponseEntity<String> reHandler(UnauthorizedUser e) {
+        return new ResponseEntity<>("RuntimeException in throwExceptionIne method", HttpStatus.I_AM_A_TEAPOT);
     }
 }
